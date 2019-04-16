b1878924100617c02a35d06158c55da88582b468
Connor Schmidt <schmidty68connor@gmail.com>
2019-04-15 23:55:11 -0400

    added simple demo project

---
+++ DemoPlayer/Program.cs
@@ -0,0 +1,40 @@
+﻿using AbundantMusic.NET;
+using Commons.Music.Midi;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DemoPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter seed: ");
+            string seed = Console.ReadLine();
+
+            // Generate a MIDI using Abundant Music
+            Console.WriteLine("Generating Midi...");
+            byte[] midiFile = MidiComposer.Generate(seed);
+
+            // Play the MIDI using managed-midi
+            // https://github.com/atsushieno/managed-midi
+            var access = MidiAccessManager.Default;
+            var music = MidiMusic.Read(new MemoryStream(midiFile));
+            using (var player = new MidiPlayer(music, access))
+            {
+                long totalPlayTime = player.GetTotalPlayTimeMilliseconds();
+                Console.WriteLine("Play time: " + TimeSpan.FromMilliseconds(totalPlayTime).ToString("g"));
+
+                player.Play();
+                while (player.State == PlayerState.Playing)
+                {
+
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
